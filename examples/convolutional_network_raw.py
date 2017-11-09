644835abb5b947621e8b1e39dba981dc6c7de32a
enningxie <enningxie@163.com>
2017-11-09 09:32:22 +0800

    cnn_raw

---
+++ examples/convolutional_network_raw.py
@@ -0,0 +1,97 @@
+# Bulid a convolutional neural network with Tensorflow
+
+from __future__ import division, print_function, absolute_import
+
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
+
+learning_rate = 0.001
+num_steps = 10000
+batch_size = 128
+display_step = 100
+
+num_input = 784
+num_classes = 10
+dropout = 0.75
+
+X = tf.placeholder(tf.float32, [None, num_input])
+Y = tf.placeholder(tf.float32, [None, num_classes])
+keep_prob = tf.placeholder(tf.float32)  # dropout(keep probability)
+
+
+def conv2d(x, W, b, strides=1):
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
+def maxpool2d(x, k=2):
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+
+
+def conv_net(x, weights, biases, dropout):
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    conv1 = maxpool2d(conv1, k=2)
+
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    conv2 = maxpool2d(conv2, k=2)
+
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
+
+
+# Store layers weight & bias
+weights = {
+    # 5x5 conv, 1 input, 32 outputs
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    # 5x5 conv, 32 inputs, 64 outputs
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    # fully connected, 7*7*64 inputs, 1024 outputs
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    # 1024 inputs, 10 outputs (class prediction)
+    'out': tf.Variable(tf.random_normal([1024, num_classes]))
+}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+
+# Construct model
+logits = conv_net(X, weights, biases, keep_prob)
+prediction = tf.nn.softmax(logits)
+
+# Define loss and optimizer
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss_op)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.global_variables_initializer()
+
+# Start training
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(1, num_steps+1):
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        _ = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y, keep_prob: dropout})
+
+        if step % display_step == 0 or step == 1:
+            acc, loss = sess.run([accuracy, loss_op], feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
+            print("Step ", str(step), ": loss: %.4f" % loss, ", acc: %.4f" % acc)
+    test_acc = sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1.0})
+    print("Test's accuracy: ", test_acc, "\nfinish!")
+

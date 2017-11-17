3d568b0980fc1e16854a04903a6e6c42cae7da4b
enningxie <enningxie@163.com>
2017-11-17 22:37:02 +0800

    x

--- Tutorials/linearRegression.py
+++ Tutorials/linearRegression.py
@@ -15,3 +15,5 @@ theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
 with tf.Session() as sess:
     theta_value = theta.eval()
     print(theta_value)
+
+tf.reset_default_graph()

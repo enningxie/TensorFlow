0b937ebd5a332e04979ec2f43ac9908d3b2198b5
enningxie <enningxie@163.com>
2018-03-31 22:07:43 +0800

    x

---
+++ stanford/Lecture2_01.py
@@ -0,0 +1,23 @@
+# tensorboard
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+# tensorboard
+# a = tf.constant(2, name='a')
+# b = tf.constant(3, name='b')
+# x = tf.add(a, b, name='add')
+#
+# # writer = tf.summary.FileWriter('../graphs/lecture02', tf.get_default_graph())
+#
+# with tf.Session() as sess:
+#     writer = tf.summary.FileWriter('../graphs/lecture02', sess.graph)
+#     print(sess.run(x))
+#
+# writer.close()
+
+# Math operations
+a = tf.constant([2, 2], name='a')
+b = tf.constant([[0, 1], [2, 3]], name='b')
+
+with tf.Session() as sess:
